9f0d3075e877789c8d37e8ebac2c7d8ea89c0500
GitHub Wokann <Kenny_chen1998@outlook.com>
2024-05-12 23:14:31 +0800

    字体更新

--- graphic/graphic.s
+++ graphic/graphic.s
@@ -3,17 +3,15 @@
 ;替换从shiftJIs码表亚开始的汉字，可用空间总共为2965个汉字
 ;新字库汉字字模
 .org 0x6A8C
-    ;原版汉字字体为华综体，本次也使用该字体
-    ;.incbin ".\graphic\font\chinese_char_tile.2bpp"
-    ;字深
-    ;.incbin ".\graphic\font\chinese_char_tile_hunhou.2bpp"
-    ;.incbin ".\graphic\font\chinese_char_tile_ruili.2bpp"
-    ;字浅(接近原版字体深浅)
-    ;.incbin ".\graphic\font\chinese_char_tile_pinghua.2bpp"
-    .incbin ".\graphic\font\chinese_char_tile_xili.2bpp"
-;新字库汉字字模统一宽度相关数据为：01 0A 0C
+    ;原版汉字字体为华综体，本次也使用该字体(原版为10x11，但对汉字来说有点辨识度低，故加入扩大版11x11)
+    ;.incbin ".\graphic\font\chinese_char_tile_originhuazong_10x11.2bpp"
+    .incbin ".\graphic\font\chinese_char_tile_originhuazong_11x11.2bpp"
+    ;点阵体
+    ;.incbin ".\graphic\font\chinese_char_tile_piexlsongti_11x11.2bpp"
+;新字库汉字字模统一宽度相关数据为：start width length: 01 0A 0C (10x11) 00 0B 0C (11x11)
 .org 0x2146C
-    .incbin ".\graphic\font\chinese_char_width.bin"
+    ;.incbin ".\graphic\font\chinese_char_width_10x11.bin"
+    .incbin ".\graphic\font\chinese_char_width_11x11.bin"
 .close
 
 .create ".\.temp\root\res\obj_input02.NCGR",0
@@ -28,14 +26,6 @@
     .incbin ".\graphic\res\obj_reset.NCGR"
 .close
 
-.create ".\.temp\root\res\obj_return.NCER",0
-    .incbin ".\graphic\res\obj_return.NCER"
-.close
-/* 错误的文件，无需添加
-.create ".\.temp\root\res\obj_return.NCGR",0
-    .incbin ".\graphic\res\obj_return.NCGR"
-.close
-*/
 .create ".\.temp\root\res\obj_sort_button.NCGR",0
     .incbin ".\graphic\res\obj_sort_button.NCGR"
 .close
